9f24e1f7607f79e2cf4ee417aeeff3e2e61170ae
lanzhao <lanzhao@thoughtworks.com>
2021-11-03 21:06:17 +0800

    add goback

--- src/pages/commodityDetail/index.tsx
+++ src/pages/commodityDetail/index.tsx
@@ -1,14 +1,18 @@
 import React, {useEffect, } from 'react'
-import {useParams} from 'react-router-dom'
+import {useHistory, useParams} from 'react-router-dom'
 import style from "./index.less";
 import {CommodityResponse, getCommodityDetail} from "../../service/commodityService";
 import Loading from "../../components/loading";
 import useRequest from "../../hooks/useRequest";
 import {CommonResponse} from "../../service/http";
+import { Link } from 'react-router-dom';
+
 
 
 function CommodityDetail() {
     const params = useParams<{ id: string }>();
+    const history = useHistory();
+
     const getDetail = (): Promise<CommonResponse<CommodityResponse>> => {
         return getCommodityDetail(params.id)
     }
@@ -19,14 +23,11 @@ function CommodityDetail() {
         execute()
     }, [params.id])
 
-
-
-
     return (
         <>
             <Loading show={loading}/>
             {detail && (<div className={style["container"]}>
-
+                <Link to={'/'} className={style["nav"]}>回到上一页</Link>
                 <img src={detail.imageUrl} alt={detail.name} className={style["image-content"]}/>
                 <div className={style["commodity-content"]}>
                     <div className={style["name"]}>
@@ -35,6 +36,7 @@ function CommodityDetail() {
                         )
                     </div>
                     <div className={style["price"]}>¥{detail.price}</div>
+
                 </div>
 
             </div>)}
